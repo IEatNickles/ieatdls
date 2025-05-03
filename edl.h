65eaf83e7b0ecd995b410a65fa94e671ae157de4
jdw <stopmotionbros777@gmail.com>
2025-05-03 09:55:51 -0500

    Final commit

--- edl.h
+++ edl.h
@@ -2,13 +2,13 @@
 
 #ifdef _WIN32
 #include <windows.h>
-#elif unix
+#else
 #include <dlfcn.h>
 #endif
 
 typedef void *DLHandle;
 
-DLHandle edl_open_dl(const char *path) {
+inline DLHandle edl_open_dl(const char *path) {
   DLHandle handle = NULL;
 
 #ifdef _WIN32
@@ -20,26 +20,26 @@ DLHandle edl_open_dl(const char *path) {
   return handle;
 }
 
-DLHandle edl_load_symbol(DLHandle handle, const char *name) {
+inline DLHandle edl_load_symbol(DLHandle handle, const char *name) {
   DLHandle sym = NULL;
 #ifdef _WIN32
   sym = GetProcAddress(handle, name);
-#elif unix
+#else
   sym = dlsym(handle, name);
 #endif
 
   return sym;
 }
 
-void edl_close_dl(DLHandle handle) {
+inline void edl_close_dl(DLHandle handle) {
 #ifdef _WIN32
   FreeLibrary(handle);
-#elif unix
+#else
   dlclose(handle);
 #endif
 }
 
-const char *edl_error() {
+inline const char *edl_error() {
 #ifdef _WIN32
   return GetLastError();
 #else
